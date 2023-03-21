9de3ba1f7fb2eec9c8b2aded8c9e7fd454b879c7
Rikes8 <henrique19costa@gmail.com>
2023-03-21 14:45:23 +0000

    Blocking Queue. Indexar urls submetidos pelo cliente na queue.

--- src/Queue.java
+++ src/Queue.java
@@ -1,12 +1,19 @@
 package src;
 
+
 import java.net.*;
 import java.io.*;
+import java.util.concurrent.*;
 
 public class Queue {
     private static int serverPort = 6000;
 
+    //FIXME: QUEUE
+    public static LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
     public static void main(String args[]){
+
+
         int numero=0;
 
         try (ServerSocket listenSocket = new ServerSocket(serverPort)) {
@@ -16,7 +23,7 @@ public class Queue {
                 Socket clientSocket = listenSocket.accept(); // BLOQUEANTE
                 System.out.println("CLIENT_SOCKET (created at accept())="+clientSocket);
                 numero++;
-                new Connection(clientSocket, numero);
+                new Connection(clientSocket, numero, queue);
             }
         } catch(IOException e) {
             System.out.println("Listen:" + e.getMessage());
@@ -27,27 +34,35 @@ public class Queue {
 //= Thread para tratar de cada canal de comunicação com um cliente
 class Connection extends Thread {
     DataInputStream in;
-    DataOutputStream out;
+    //DataOutputStream out;
     Socket clientSocket;
     int thread_number;
+    LinkedBlockingQueue<String> queue;
 
-    public Connection (Socket aClientSocket, int numero) {
+
+    public Connection (Socket aClientSocket, int numero, LinkedBlockingQueue<String> fifo) {
         thread_number = numero;
         try{
             clientSocket = aClientSocket;
             in = new DataInputStream(clientSocket.getInputStream());
-            out = new DataOutputStream(clientSocket.getOutputStream());
+            //out = new DataOutputStream(clientSocket.getOutputStream());
+            queue = fifo;
             this.start();
         }catch(IOException e){System.out.println("Connection:" + e.getMessage());}
     }
     //=============================
     public void run(){
-        String resposta;
+
         try {
             //while(true){
                 //an echo server
                 String data = in.readUTF();
                 System.out.println("T[" + thread_number + "] Recebeu: "+data);
+
+                //add urls to linked list
+                queue.add(data);
+                System.out.println("queue contains " + queue);
+
                 //out.writeUTF(data);
             //}
         } catch(EOFException e) {
